f1fbc05b7656473e2c46cf3c203b760b48083fa7
Saulo Lins Dias <saulo.dias@ntconsult.com.br>
2025-07-28 01:44:00 -0300

    fix: update output file paths in package.json and enhance Vite config with type definitions

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/libs/index.ts'),
+      name: 'NACoreUI',
+      fileName: (format) => `na-core-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
